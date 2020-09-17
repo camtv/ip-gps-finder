252eb461c512c83ddfe3f660ddbbc24aa5121898
testcam <admin@cam.tv>
2020-09-17 06:51:29 +0000

    client

--- Client/index.js
+++ Client/index.js
@@ -3,14 +3,21 @@ import "./index.scss";
 
 export class MapComponent extends LitElement {
 
+    // TODO: 
+    //  - Creare un componente mappa che prenda tutta l'area del contenitore e visualizzi una mappa di google
+    //  - Usare gli oggetto mappa google o creare un componente "hotPoint" che visualizza sulla mappa un tag ad una locazione stabilita
+    //  - Caricare gli hotPoint tramite chiamata alla Api Locations
+
+
     static get properties() {
         return {
-            prop: { type: String },
+            nome_proprieta: { type: String },
         };
     }
 
     constructor() {
         super();
+        this.nome_proprieta = "";
 
     }
 
@@ -21,18 +28,18 @@ export class MapComponent extends LitElement {
     render() {
             return html`                
                 <div>
-                    Componente mappa  ${this.prop}                   
+                    Componente mappa  ${this.nome_proprieta}                   
                 </div>  
             `;
     }
 
-    firstUpdated() {
+ //   firstUpdated() {
 
-    }
+ //   }
 
-    updated(changedProperties) {
+ //   updated(changedProperties) {
 
-	}
+	//}
 
 }
 
